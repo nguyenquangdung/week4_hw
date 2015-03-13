4a6f8acf084f08b34aba1ac97ff78f220364ed06
Dung <dungnq164@gmail.com>
2015-03-13 14:16:21 +0700

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://sirelwm.com/wp-content/uploads/2015/02/yahoo-messenger-icon-512x512.png"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
